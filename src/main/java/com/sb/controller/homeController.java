fe2d1cecdaefa7d3493d0923d6f09b221fc44bf3
woobc <longnun@naver.com>
2024-05-28 14:37:51 +0900

    add CORS Policy

--- src/main/java/com/sb/controller/homeController.java
+++ src/main/java/com/sb/controller/homeController.java
@@ -1,10 +1,12 @@
 package com.sb.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class homeController {
     private final com.sb.service.homeService homeService;
